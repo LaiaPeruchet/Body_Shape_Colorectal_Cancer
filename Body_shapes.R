4ac9b57282fc1ac0d228c2e1cbc35faeb1df472a
LaiaPeruchet <146433032+LaiaPeruchet@users.noreply.github.com>
2023-11-01 15:44:59 +0100

    Body_shapes.R

--- Body_shapes.R
+++ Body_shapes.R
@@ -1,16 +1,16 @@
 
-### Body Shape Phenotypes
+# Body Shape Phenotypes
 
-# Load data and select the variables of interest
+## Load data and select the variables of interest
 pheno_data <- data[,c("id", "age_rec", "sex", "centre", "Height", "Weight", "BMI", "Waist", "Hip", "WHR")]
 pheno_data_na <- na.omit(pheno_data) #Remove missing values
 
-# Principal component analysis
+## Principal component analysis (PCA)
 pca1 <- select(pheno_data_na, Height, Weight, BMI, age_rec, sex, centre, id)
 pca2 <- select(pheno_data_na, Hip, Waist, WHR, age_rec, sex, centre, id)
 pca2 <- pca2[complete.cases(pca2),]
 
-# Regression for obtaining stansardized residuals
+## Obtain stansardized residuals
 height <- lm(Height ~ age_rec + sex + centre, data = pca1)
 pca1$res.height <- rstandard(height)
 
@@ -29,10 +29,10 @@ pca2$res.waist <- rstandard(waist)
 whr <- lm(WHR ~ age_rec + sex + centre, data = pca2)
 pca2$res.whr <- rstandard(whr)
 
-# Left join of pc1 and pc2
+## Left join of pc1 and pc2
 pca <- left_join(pca1, pca2[c("id", "res.hip", "res.waist", "res.whr", "Hip", "Waist", "WHR")], by="id")
 summary(pca$res.bmi)
 
-# Complete case PCA
+## Complete case PCA
 pca_complete <- pca[complete.cases(pca),]
 results <- prcomp(pca_complete[,8:13], scale. = T, center = T)
